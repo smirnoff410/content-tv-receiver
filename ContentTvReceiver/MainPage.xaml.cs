348d66c0bbb95a1036965fc38a6fad80888f8aca
vladislav.smirnov <vladislav.smirnov@singularis-lab.com>
2022-08-25 21:07:45 +0300

    first version

--- ContentTvReceiver/MainPage.xaml.cs
+++ ContentTvReceiver/MainPage.xaml.cs
@@ -1,25 +1,84 @@
-﻿namespace ContentTvReceiver;
+﻿using System.IO;
+using System.IO.Pipes;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ContentTvReceiver;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    byte[] encryptedContent;
 
-	public MainPage()
+
+    public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        var bot = new TelegramBotClient("5539542686:AAFdCUf9ze542bqugblAe3pgK6XoZtB4yaU");
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { }, // receive all update types
+        };
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken);
+    }
+
+    async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Некоторые действия
+        Console.WriteLine(JsonConvert.SerializeObject(update));
+        if (update.Type == UpdateType.Message)
+        {
+            var message = update.Message;
+            if (message.Type == MessageType.Photo)
+            {
+                await SaveImage(botClient, message.Photo.Last().FileId);
+            }
+            else if (message.Type == MessageType.Document)
+            {
+                if (message.Document.MimeType == "video/mp4")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "К сожалению я пока что не могу отображать анимированные изображения :(");
+                    return;
+                }
+                await SaveImage(botClient, message.Document.FileId);
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Вы отправили неизвестный мне формат, я не знаю как это отобразить :(");
+                return;
+            }
+            await botClient.SendTextMessageAsync(message.Chat, "Изображение успешно принято и скоро отобразится :)");
+        }
+    }
+
+    async Task SaveImage(ITelegramBotClient botClient, string fileId)
+    {
+
+        var file = await botClient.GetFileAsync(fileId);
+
+        using (var fileStream = new MemoryStream())
+        {
+            await botClient.DownloadFileAsync(file.FilePath, fileStream);
+            encryptedContent = fileStream.ToArray();
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ImageContent.Source = ImageSource.FromStream(() => new MemoryStream(encryptedContent));
+            });
+        }
+    }
+
+    async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        // Некоторые действия
+        Console.WriteLine(JsonConvert.SerializeObject(exception));
+    }
 }
 
 
---
+++ TelegramBotReceiver/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using File = System.IO.File;
+
+var bot = new TelegramBotClient("5539542686:AAFdCUf9ze542bqugblAe3pgK6XoZtB4yaU");
+
+var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = { }, // receive all update types
+};
+
+bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken);
+
+Console.WriteLine("Telegram bot receiver starting");
+Console.ReadLine();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Некоторые действия
+    Console.WriteLine(JsonConvert.SerializeObject(update));
+    if (update.Type == UpdateType.Message)
+    {
+        var message = update.Message;
+        if(message.Type == MessageType.Photo)
+        {
+            await SaveImage(botClient, message.Photo.Last().FileId);
+        }
+        else if (message.Type == MessageType.Document)
+        {
+            if (message.Document.MimeType == "video/mp4")
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "К сожалению я пока что не могу отображать анимированные изображения :(");
+                return;
+            }
+            await SaveImage(botClient, message.Document.FileId);
+        }
+        await botClient.SendTextMessageAsync(message.Chat, "Изображение успешно принято и скоро отобразится :)");
+    }
+}
+
+async Task SaveImage(ITelegramBotClient botClient, string fileId)
+{
+
+    var file = await botClient.GetFileAsync(fileId);
+
+    byte[] encryptedContent;
+    using (var fileStream = new MemoryStream())
+    {
+        await botClient.DownloadFileAsync(file.FilePath, fileStream);
+        encryptedContent = fileStream.ToArray();
+    }
+    File.WriteAllBytes("image.mp4", encryptedContent);
+}
+
+async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    // Некоторые действия
+    Console.WriteLine(JsonConvert.SerializeObject(exception));
+}
+
